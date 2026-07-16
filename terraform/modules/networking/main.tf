ab6c3ce8b65357f3f47a8ada8eb79d4bd1b029f6
Jahirul Islam <jahirul.islam200518@gmail.com>
2026-07-16 14:17:31 +0100

    added vpc module

---
+++ terraform/modules/networking/main.tf
@@ -0,0 +1,28 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "main"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = false
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb"               = "true"
+    "kubernetes.io/cluster/production-eks" = "shared"
+
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb"      = "true"
+    "kubernetes.io/cluster/production-eks" = "shared"
+
+  }
+
+}
