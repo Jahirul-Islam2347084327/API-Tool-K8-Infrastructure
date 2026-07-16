e3eb6274566052145b17a8a85dfd4058c98457d6
Jahirul Islam <jahirul.islam200518@gmail.com>
2026-07-16 20:58:19 +0100

    setup main apply file

--- terraform/workflows/development/backend-infra/main.tf
+++ terraform/workflows/development/backend-infra/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "jahis-devops-directive-state-2026"
-    key = "backend-infra/terraform.tfstate"
+    key = "eks-backend/terraform.tfstate"
     region = "us-east-1" # change this to your desired region
     dynamodb_table = "terraform-state-locking"
     encrypt = true
@@ -24,14 +24,40 @@ provider "aws" {
 
 provider "helm" {
     
-  kubernetes = {
+  kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
-    exec = {
+    exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
     }
+    }
+
+    module "eks" {
+      source = "../../../modules/eks"
+      vpc-id = module.network.vpc_id
+      private-subnets = module.network.private_subnets
+    }
+
+    module "k8-apps" {
+      source = "../../../modules/k8-apps"
+      external-dns-iam = module.iam.external-iam-arn
+      cert-manager-iam = module.iam.cert-iam-arn
+    }
+
+    module "network" {
+      source = "../../../modules/networking"
+    }
+
+    module "iam" {
+      source = "../../../modules/iam"
+      cluster-oidc-issuer-url = module.eks.cluster_oidc_issuer_url
+      oidc-provider-arn = module.eks.aws_iam_openid_connect_provider
+    }
+
+    module "ecr" {
+      source = "../../../modules/ecr"
     }
