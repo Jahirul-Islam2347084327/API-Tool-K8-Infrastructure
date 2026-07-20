6f62570a6e580e8cf95234f36d2347eabb86eb8d
Jahirul Islam <jahirul.islam200518@gmail.com>
2026-07-20 20:24:11 +0100

    config fix

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -6,7 +6,7 @@ module "vpc" {
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24","10.0.3.0/24"]
-  public_subnets  = ["10.0.5.0/24", "10.0.5.0/24","10.0.6.0/24"]
+  public_subnets  = ["10.0.5.0/24", "10.0.6.0/24","10.0.7.0/24"]
 
   enable_nat_gateway   = true
   single_nat_gateway   = false
